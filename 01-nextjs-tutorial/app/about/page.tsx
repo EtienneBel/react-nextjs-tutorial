5532660e3c13744d3e2da65a8c49c5559aeeb5d2
Etienne Wend-Kuuni Belemgnegre <ebelemgnegre@oseogroup.com>
2024-06-25 01:20:42 +0000

    added: home-away project

---
+++ 01-nextjs-tutorial/app/about/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function AboutPage() {
+    return (
+        <div>
+            <h1 className='text-7xl'>
+                AboutPage
+            </h1>
+        </div>
+    )
+}
+
+export default AboutPage
---
+++ 02-home-away/app/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function HomeAway() {
+  return (
+    <div>HomeAway</div>
+  )
+}
+
+export default HomeAway
