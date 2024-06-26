28f11617f80d67885f7b5cf77277fafcd234d65a
Etienne <belemgnegreetienne@gmail.com>
2024-06-26 12:53:23 +0000

    adding: useReducer to quiz state management

--- 02-react-quiz/src/App.js
+++ 02-react-quiz/src/App.js
@@ -1,8 +1,48 @@
-import React from "react";
-import DateCounter from "./DateCounter";
+import React, { useEffect, useReducer } from "react";
+import Header from "./Header";
+import Main from "./Main";
+
+const initialState = {
+  questtions: [],
+  status: "loading",
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "dataReceived":
+      return {
+        ...state,
+        questtions: action.payload,
+        status: "ready",
+      };
+    case "dataFailed":
+      return {
+        ...state,
+        status: "error",
+      };
+    default:
+      throw new Error("Action unknown");
+  }
+}
 
 function App() {
-  return <DateCounter />;
+  const [state, dispatch] = useReducer(reducer, initialState);
+  useEffect(function () {
+    fetch("http://localhost:8080/questions")
+      .then((result) => result.json())
+      .then((data) => dispatch({ type: "dataReceived", payload: data }))
+      .catch((err) => dispatch({ type: "dataFailed" }));
+  }, []);
+
+  return (
+    <div>
+      <Header />
+      <Main>
+        <p>1/5</p>
+        <p>Question ?</p>
+      </Main>
+    </div>
+  );
 }
 
 export default App;
